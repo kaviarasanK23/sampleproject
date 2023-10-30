9737e644b2e75cd9e0c3caf2fffeb5927fe6c944
kaviarasanK23 <147176312+kaviarasanK23@users.noreply.github.com>
2023-10-30 22:10:54 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,10 @@
+package first;
+
+public class sample {
+    public static void main(String[]args){
+        int a=10,b=45;
+        int add=a+b;
+        System.out.println("added values:"+add);
+    }
+    
+}
